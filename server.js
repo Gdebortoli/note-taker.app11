bd4e612259b510e64fbd4e25170df825f7693fd4
“Gigi <debortolg@gmail.com>
2023-01-06 12:44:27 -0800

    adding some basics to the files

--- server.js
+++ server.js
@@ -4,16 +4,24 @@ const fs = require('fs');
 const path = require('path'); 
 // Requiring Express 
 const express = require('express'); 
+
 // Requiring 
 
 // Instantiating Server (step 1)
 const app = express(); 
 const PORT = process.env.PORT || 3001; 
-
 // Parse incoming JSON data
 app.request(express.json());
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// JS AND CSS file middleware
+app.use(express.static('public'));
+
+// Queries 
+
+
+
+// Validations 
 
 
 
@@ -25,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-// Ask server to listen for requests (step 2)
+// Ask server to listen for requests (step 2 - stays last on the page)
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
